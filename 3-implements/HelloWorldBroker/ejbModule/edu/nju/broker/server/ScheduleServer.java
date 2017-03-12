5d7d449115860f168d6113eeabe58f3501581378
Carter1996 <wangjc19960926@163.com>
2017-03-12 19:31:26 +0800

    放错了

--- 3-implements/HelloWorldBroker/ejbModule/edu/nju/broker/server/ScheduleServer.java
+++
@@ -1,8 +0,0 @@
-package edu.nju.broker.server;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface ScheduleServer {
-	public String findRoute(String routeId);
-}
--- 3-implements/HelloWorldBroker/ejbModule/edu/nju/broker/stateless/ScheduleService.java
+++
@@ -1,8 +0,0 @@
-package edu.nju.broker.stateless;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface ScheduleService {
-	public String findRoute(String routeId);
-}
